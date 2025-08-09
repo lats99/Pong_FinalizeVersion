65c30b3815d2a3643162fc5b962afb7dfd24ca1c
lats99 <faridf.taufiq@gmail.com>
2025-08-09 13:48:56 +0700

    init

---
+++ scenes/pause_menu.gd
@@ -0,0 +1,41 @@
+extends Control
+
+func _ready() -> void:
+	$AnimationPlayer.play("RESET")
+
+func resume():
+	get_tree().paused = false
+	$AnimationPlayer.play_backwards("blur")
+func pause():
+	get_tree().paused = true
+	$AnimationPlayer.play('blur')
+func testEsc():
+	if Input.is_action_just_pressed("esc") and get_tree().paused == false:
+		pause()
+	elif Input.is_action_just_pressed("esc") and get_tree().paused == true:
+		resume()
+		
+
+func _on_resume_pressed() -> void:
+	resume()
+
+
+func _on_restart_pressed() -> void:
+	resume()
+	get_tree().reload_current_scene()
+
+
+func _on_option_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_main_menu_pressed() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file('res://scenes/menu.tscn')
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+func _process(_delta: float) -> void:
+	testEsc()
