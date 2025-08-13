b43598f8b619512021bbbf11ba7720990badd51c
lats99 <faridf.taufiq@gmail.com>
2025-08-13 13:47:56 +0700

    Debug option menu

--- scenes/menu.gd
+++ scenes/menu.gd
@@ -3,15 +3,17 @@ extends Control
 @onready var option: CanvasLayer = $Option
 
 func _ready() -> void:
-	option.visible = false
+	#option.visible = false
+	option.hide()
 
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/pong.tscn")
 
-
 func _on_options_pressed() -> void:
-	option.visible = true
-
+	option.show()
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+func _on_option_menu_close() -> void:
+	option.hide()
--- scenes/option_menu.gd
+++ scenes/option_menu.gd
@@ -1,7 +1,7 @@
 extends Control
-@onready var option: CanvasLayer = $".."
-@onready var option_layer: CanvasLayer = $".."
+#@onready var option: CanvasLayer = $".."
+#@onready var option_layer: CanvasLayer = $".."
+signal close
 
 func _on_x_button_pressed() -> void:
-	option.visible = false
-	option_layer.visible = false
+	close.emit()
